a6de21e12871a0d4db107e60414ddfec8dad94f2
Vilma Kaijser <vilma.kaijser@gmail.com>
2021-02-22 19:11:03 +0100

    inloggad loop, beställningar, och anropa addToCart

--- src/Order.java
+++ src/Order.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.util.List;
+
 /**
  * Created by Lisa Ramel
  * Date: 2021-02-22
@@ -7,4 +10,31 @@
  */
 public class Order {
 
+    private int id;
+    private LocalDate date;
+    private int customerId;
+    private Customer customer;
+    private List<Integer> productIds;
+
+    public Order(int id, String date, int customerId) {
+        this.id = id;
+        this.date = LocalDate.parse(date);
+        this.customerId = customerId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public List<Integer> getProductIds() {
+        return productIds;
+    }
+
+    public void setProductIds(List<Integer> productIds) {
+        this.productIds = productIds;
+    }
 }
--- src/Repository.java
+++ src/Repository.java
@@ -17,113 +17,163 @@ public class Repository {
 
     private Properties p = new Properties();
 
-        public Repository(){
+    public Repository() {
 
-            try {
-                p.load(new FileInputStream("src/resources/Settings.properties"));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-
+        try {
+            p.load(new FileInputStream("src/resources/Settings.properties"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        public List<Product> getProducts(){
-            List<Product> products = new ArrayList<>();
 
-            String query = "select id, euSizing, colour, price, brandId, amountInStock from product";
+    }
 
-            try(Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
-                Statement stmt = con.createStatement();
-                ResultSet rs = stmt.executeQuery(query)){
+    public List<Product> getProducts() {
+        List<Product> products = new ArrayList<>();
 
-                while(rs.next()){
-                    Product p = new Product(rs.getInt("id"), rs.getInt("euSizing"),
-                            rs.getString("colour"), rs.getDouble("price"),
-                            rs.getInt("brandId"), rs.getInt("amountInStock"));
+        String query = "select id, euSizing, colour, price, brandId, amountInStock from product";
 
-                    products.add(p);
+        try (Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            while (rs.next()) {
+                Product p = new Product(rs.getInt("id"), rs.getInt("euSizing"),
+                        rs.getString("colour"), rs.getDouble("price"),
+                        rs.getInt("brandId"), rs.getInt("amountInStock"));
+
+                products.add(p);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return products;
+
+    }
+
+    public List<Object> getTables(String tableName) {
+        List<Object> list = new ArrayList<>();
+        String query = "select id, name from ?";
+
+        try (Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
+             CallableStatement stmt = con.prepareCall(query)) {
+
+            ResultSet rs = stmt.executeQuery(query);
+
+            stmt.setString(1, tableName);
+            rs = stmt.executeQuery();
+
+            if (tableName.equals("brand")) {
+                while (rs.next()) {
+                    Brand b = new Brand(rs.getInt("id"),
+                            rs.getString("name"));
+                    list.add(b);
                 }
-
-            }catch (SQLException e){
-                e.printStackTrace();
-            }
-
-            return products;
-
-        }
-
-        public List<Object> getTables(String tableName){
-            List<Object> list = new ArrayList<>();
-            String query = "select id, name from ?";
-
-            try(Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
-                CallableStatement stmt = con.prepareCall(query)){
-
-                ResultSet rs = stmt.executeQuery(query);
-
-                stmt.setString(1, tableName);
-                rs = stmt.executeQuery();
-
-                if(tableName.equals("brand")){
-                    while(rs.next()){
-                        Brand b = new Brand(rs.getInt("id"),
-                                rs.getString("name"));
-                        list.add(b);
-                    }
-                } else if (tableName.equals("category")){
-                    while(rs.next()){
+            } else if (tableName.equals("category")) {
+                while (rs.next()) {
                     Category c = new Category(rs.getInt("id"),
                             rs.getString("name"));
                     list.add(c);
-                }  }
-
-
-            }catch (SQLException e){
-                e.printStackTrace();
+                }
             }
-            return list;
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return list;
+    }
 
 
-    public List<Brand> getBrands(){
+    public List<Brand> getBrands() {
         List<Brand> brands = new ArrayList<>();
         String query = "select id, name from brand";
 
-        try(Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(query)){
+        try (Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
 
-            while(rs.next()){
+            while (rs.next()) {
                 Brand b = new Brand(rs.getInt("id"),
                         rs.getString("name"));
                 brands.add(b);
             }
 
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return brands;
     }
 
+    public List<Order> getOrders(int customerId) {
+        List<Order> orders = new ArrayList<>();
+        // men egentligen behöver vi väl inte customerId
+        String query = "select id, dateOfOrder, customerId from orders where customerId = ?";
 
-    public List<Category> getCategories(){
+        try (Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
+             CallableStatement stmt = con.prepareCall(query)) {
+
+            stmt.setInt(1, customerId);
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()){
+                Order o = new Order(rs.getInt("id"), rs.getString("dateOfOrder"), rs.getInt("customerId"));
+                orders.add(o);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+
+        }
+
+        return orders;
+    }
+
+    public List<Integer> getCarts(int ordersId){
+        List<Integer> productIds = new ArrayList<>();
+
+        String query = "select productId from cart where ordersId = ?";
+
+        try (Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
+             CallableStatement stmt = con.prepareCall(query)) {
+
+            stmt.setInt(1, ordersId);
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()){
+                Integer i = rs.getInt("productId");
+                productIds.add(i);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+
+        }
+
+        return productIds;
+
+
+    }
+
+    public List<Category> getCategories() {
         List<Category> categories = new ArrayList<>();
         String query = "select id, name from category";
 
-        try(Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(query)){
+        try (Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
 
-            while(rs.next()){
+            while (rs.next()) {
                 Category c = new Category(rs.getInt("id"),
                         rs.getString("name"));
                 categories.add(c);
             }
 
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return categories;
@@ -148,103 +198,102 @@ public class Repository {
         return belongings;
     }*/
 
-    public List<Customer> readCustomers(){
-            List<Customer> customers = new ArrayList<>();
+    public List<Customer> readCustomers() {
+        List<Customer> customers = new ArrayList<>();
 
-            String query = "select id, name, addressId, password, username from customer";
+        String query = "select id, name, addressId, password, username from customer";
 
 
-            try(Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
-                Statement stmt = con.createStatement();
-                ResultSet rs = stmt.executeQuery(query)){
+        try (Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
 
-                while(rs.next()){
-                    Customer c = new Customer(rs.getInt("id"), rs.getString("name"),
-                            rs.getInt("addressId"), rs.getString("password"),
-                            rs.getString("username"));
+            while (rs.next()) {
+                Customer c = new Customer(rs.getInt("id"), rs.getString("name"),
+                        rs.getInt("addressId"), rs.getString("password"),
+                        rs.getString("username"));
 
-                    customers.add(c);
-                }
-
-            }catch (SQLException e){
-                e.printStackTrace();
+                customers.add(c);
             }
 
-            return customers;
-
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
-        public String newOrder(int customerId, int ordersID, int productId){
-            ResultSet rs = null;
-            String errorMsg = ""; // används för att ta emot select-sats med felmeddelande från sp
-            String query = "CALL addToCart(?,?,?)";
-
-            try(Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
-                CallableStatement stmt = con.prepareCall(query)) {
-
-                stmt.setInt(1, customerId);
-                stmt.setInt(2, ordersID);
-                stmt.setInt(3, productId);
-                rs = stmt.executeQuery();
-
-                while(rs != null && rs.next()){
-                    errorMsg = rs.getString("error");
-                }
-                if(!errorMsg.equals("")){
-                    return errorMsg;
-                }
-
-
-
-            } catch (Exception e){
-                e.printStackTrace();
-
-
-            }
-            return "Ny order!";
-        }
-
-        public Customer findCustomer(String userName){
-
-            String query = "select * from customer where username like " + '\'' + userName + '\'';
-
-            try(Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
-                Statement stmt = con.createStatement();
-                ResultSet rs = stmt.executeQuery(query)){
-
-                Customer c = null;
-
-                while (rs.next()) {
-                    c = new Customer(rs.getInt("id"), rs.getString("name"), rs.getInt("addressId"), rs.getString("username"), rs.getString("password"));
-                }
-                return c;
-
-            }catch (SQLException e){
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        public Customer signIn(String userName, String password){
-
-            String query = "select * from customer where username like " + '\'' + userName + '\'' + " and password like " + '\'' + password + '\'';
-
-            try(Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
-                Statement stmt = con.createStatement();
-                ResultSet rs = stmt.executeQuery(query)){
-
-                Customer c = null;
-
-                while (rs.next()) {
-                    c = new Customer(rs.getInt("id"), rs.getString("name"), rs.getInt("addressId"), rs.getString("username"), rs.getString("password"));
-                }
-                return c;
-
-
-            }catch (SQLException e){
-                e.printStackTrace();
-            }
-            return null;
-        }
+        return customers;
 
     }
+
+    public String newOrder(int customerId, int ordersID, int productId) {
+        ResultSet rs = null;
+        String errorMsg = ""; // används för att ta emot select-sats med felmeddelande från sp
+        String query = "CALL addToCart(?,?,?)";
+
+        try (Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
+             CallableStatement stmt = con.prepareCall(query)) {
+
+            stmt.setInt(1, customerId);
+            stmt.setInt(2, ordersID);
+            stmt.setInt(3, productId);
+            rs = stmt.executeQuery();
+
+            while (rs != null && rs.next()) {
+                errorMsg = rs.getString("error");
+            }
+            if (!errorMsg.equals("")) {
+                return errorMsg;
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+
+        }
+        return "Ny order!";
+    }
+
+    public Customer findCustomer(String userName) {
+
+        String query = "select * from customer where username like " + '\'' + userName + '\'';
+
+        try (Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            Customer c = null;
+
+            while (rs.next()) {
+                c = new Customer(rs.getInt("id"), rs.getString("name"), rs.getInt("addressId"), rs.getString("username"), rs.getString("password"));
+            }
+            return c;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Customer signIn(String userName, String password) {
+
+        String query = "select * from customer where username like " + '\'' + userName + '\'' + " and password like " + '\'' + password + '\'';
+
+        try (Connection con = DriverManager.getConnection(p.getProperty("connectionString"), p.getProperty("name"), p.getProperty("password"));
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            Customer c = null;
+
+            while (rs.next()) {
+                c = new Customer(rs.getInt("id"), rs.getString("name"), rs.getInt("addressId"), rs.getString("username"), rs.getString("password"));
+            }
+            return c;
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
